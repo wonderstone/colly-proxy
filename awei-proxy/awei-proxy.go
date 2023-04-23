16bf3e3e736fe498bdc55034246e167974fc7303
wonderstone <tieqiw@hotmail.com>
2023-04-23 19:43:47 +0800

    minor adjustment

--- awei-proxy/awei-proxy.go
+++ awei-proxy/awei-proxy.go
@@ -1,9 +1,7 @@
 package aweiproxy
 
-import (
-	"fmt"
-	"time"
-)
-
 // define a variable to store the url and initialize it
 var url string = "https://agit.ai/12/a/src/branch/master/4"
+
+// regular expression to match the url
+var urlReg string = `https://agit.ai/12/a/src/branch/master/4`
